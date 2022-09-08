f227998b374cc0180e46f19de3d68cbef6f20582
hyunsungK <87116570+hyunsungK@users.noreply.github.com>
2022-09-09 02:10:26 +0900

    feat: add terraform

---
+++ terraform/install.sh
@@ -0,0 +1,21 @@
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+
+cat << EOF >> ~/.bashrc
+alias tf=terraform
+EOF
+
+source ~/.bashrc
+
+sudo apt install unzip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+rm -f awscliv2.zip
+rm -f ./aws
