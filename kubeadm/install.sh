8a234e54770c844375bf93573535eeb855deefa1
hyunsungK <87116570+hyunsungK@users.noreply.github.com>
2022-09-09 01:46:56 +0900

    Update install.sh

--- kubeadm/install.sh
+++ kubeadm/install.sh
@@ -53,3 +53,7 @@ alias k=kubectl
 EOF
 source ~/.bashrc
 
+sudo swapoff -a                 # Disable all devices marked as swap in /etc/fstab
+sudo sed -e '/swap/ s/^#*/#/' -i /etc/fstab   # Comment the correct mounting point
+sudo systemctl mask swap.target               # Completely disabled
+
